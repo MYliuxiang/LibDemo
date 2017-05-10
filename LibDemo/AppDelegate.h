edb3381c717ea5fef1ece67ab2e974fbaffd7de0
liuxiang <liuxiang@liuxiangdeMacBook-Pro.local>
2017-05-10 10:39:26 +0800

    intal

---
+++ LibDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LibDemo
+//
+//  Created by 刘翔 on 2017/5/10.
+//  Copyright © 2017年 刘翔. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
